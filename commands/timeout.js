5eb877236d1089cdcb5a0163295229d56e8b6740
BlockybombDEV <118032108+BlockybombDEV@users.noreply.github.com>
2022-11-13 02:47:47 +0100

    initial commit

---
+++ commands/timeout.js
@@ -0,0 +1,94 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChatInputCommandInteraction,EmbedBuilder, Options } = require('discord.js');
+const Database = require('../utils/infractions')
+const ms = require('ms');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('timeout')
+    .setDescription('Timeout a member.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+    .setDMPermission(false)
+    .addUserOption(options => options
+        .setName('target')
+        .setDescription('Member to timeout')
+        .setRequired(true)
+    
+    )
+    .addStringOption(options => options
+        .setName('duration')
+        .setDescription('Provide a duration to timeout (1m,1h,1d)')
+        .setRequired(true)
+    )
+    .addStringOption(options => options
+        .setName('reason')
+        .setDescription('Provide a reason for the timeout')
+        .setMaxLength(512)    
+    ),
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const { options, guild, member } = interaction;
+
+        const target = options.getMember('target');
+        const duration = options.getString('duration');
+        const reason = options.getString('reason') || "None Specified.";
+
+        const errorsArray = [];
+
+        const errorsEmbed = new EmbedBuilder().setAuthor({name: "Could not timeout member due to"})
+        .setColor('Red');
+
+        if(!target) return interaction.reply({
+            embeds: [errorsEmbed.setDescription('Member has most likely left the server.')],
+            ephemeral: true
+        });
+
+        if(!ms(duration) || ms(duration) > ms("28d"))
+        errorsArray.push('Time provided is invalid or over the 28d limit.');
+
+        if(!target.manageable || !target.moderatable)
+        errorsArray.push('Selected target is not moderatable by this bot.');
+
+        if(member.roles.highest.position < target.roles.highest.position)
+        errorsArray.push('Selected member has a higher role position than you.');
+
+        if(errorsArray.length)
+        return interaction.reply({
+            embeds: [errorsEmbed.setDescription(errorsArray.join("\n"))],
+            ephemeral: true
+        });
+
+        let timeError = false;
+        await target.timeout(ms(duration), reason).catch(() => timeError = true );
+
+        if(timeError)
+        return interaction.reply({
+            embeds:[errorsEmbed.setDescription("Could not timeout user due to an uncommon error. Cannot take negative values")],
+            ephemeral: true
+        });
+
+        const newInfractionsObject = {
+            IssuerID: member.id,
+            IssuerTag: member.user.tag,
+            Reason: reason,
+            Date: Date.now()
+        }
+
+        let userData = await Database.findOne({Guild: guild.id, User: target.id});
+        if(!userData) userData = await Database.create({Guild: guild.id, User: targer.id, Infractions: [newInfractionsObject]});
+        else userData.Infractions.push(newInfractionsObject) && await userData.save();
+
+
+        const succesEmbed = new EmbedBuilder().setAuthor({name: "Timeout issues", iconURL: guild.iconURL()})
+        .setColor('Gold')
+        .setDescription([
+            `${target} was issued a timeout for **${ms(ms(duartion), {long: true})}** by ${member}`,
+            `bringing their Timeout total to **${userData.Infractions.length} points**.`,
+            `**Reason**: ${reason}`
+        ].join("\n"));
+
+        return interaction.reply({embeds: [successEmbed]});
+    }
+}

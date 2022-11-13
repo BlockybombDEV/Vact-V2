d17f55953ec352294f21278edab932430883151b
BlockybombDEV <118032108+BlockybombDEV@users.noreply.github.com>
2022-11-13 23:05:43 +0100

    fixed rob command

--- commands/rob.js
+++ commands/rob.js
@@ -63,7 +63,7 @@ module.exports = {
 
         userData.wallet += Amount
         victimData.wallet -= Amount
-        userData.cooldowns.beg = Date.now() + (1000 * 60)
+        userData.cooldowns.steal = Date.now() + (1000 * 60)
         userData.save()
         victimData.save()
 
